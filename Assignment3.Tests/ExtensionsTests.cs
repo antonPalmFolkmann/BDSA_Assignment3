d2f61c7c36834960379ece60fcc526de59a9ec04
Philip Kristian Møller Flyvholm <phif@itu.dk>
2021-09-24 13:12:10 +0200

    Implented and tested isSecure

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -5,10 +5,17 @@ namespace BDSA2020.Assignment02.Tests
 {
     public class ExtensionsTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("https://google.com", true)]
+        [InlineData("http://google.com", false)]
+        public void URI_is_secure(String url, bool expected)
         {
+            var input = new Uri(url);
 
+            var output = input.isSecure();
+
+            Assert.Equal(output, expected);
         }
+        
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -4,5 +4,8 @@ namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+        public static bool isSecure(this Uri uri){
+            return uri.Scheme == Uri.UriSchemeHttps;
+        }
     }
 }
