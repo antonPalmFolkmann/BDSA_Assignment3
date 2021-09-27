13a779d07b2d053737793144e731c986553cbf49
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-09-27 11:20:39 +0200

    First two iterations of linq queries

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,51 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Assignment3.Tests
 {
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void Extension_WizardsByRowling()
         {
+            var input = "Rowling";
+            var query = new Queries();
+            List<String> expected = new List<string>{"Harry Potter", "Albus Dumbledore", "Lord Voldemort", "Gellert Grindlewald"};
 
+            IEnumerable<String> listOfWizards = query.getWizardsByAuthorExtension(input);
+
+            Assert.Equal(expected, listOfWizards);
+        }
+
+        [Fact]
+        public void Linq_WizardsByRowling()
+        {
+            var input = "Rowling";
+            var query = new Queries();
+            List<String> expected = new List<string>{"Harry Potter", "Albus Dumbledore", "Lord Voldemort", "Gellert Grindlewald"};
+
+            IEnumerable<String> listOfWizards = query.getWizardsByAuthorLinq(input);
+
+            Assert.Equal(expected, listOfWizards);
+        }
+
+        [Fact]
+        public void Extension_FindFirstSithLordYear()
+        {
+            var query = new Queries();
+            var expected = 1977;
+
+            Assert.Equal(expected, query.getFirstSithLordYearExtension());
+        }
+
+        [Fact]
+        public void Linq_FindFirstSithLordYear()
+        {
+            var query = new Queries();
+            var expected = 1977;
+
+            Assert.Equal(expected, query.getFirstSithLordYearLinq());
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Assignment3
 {
     public class Queries
     {
+        public IEnumerable<string> getWizardsByAuthorExtension(string author){
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Creator.Contains(author)).Select(w => w.Name);
+        }
+
+        public IEnumerable<string> getWizardsByAuthorLinq(string author)
+        {
+            var wizards = Wizard.Wizards.Value;
+            return from w in wizards
+                   where w.Creator.Contains(author)
+                   select w.Name;
+        }
+
+        public int? getFirstSithLordYearExtension()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Name.Contains("Darth")).OrderBy(w => w.Year).First().Year;
+        }
+
+        public int? getFirstSithLordYearLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return (from w in wizards
+                where w.Name.Contains("Darth")
+                orderby w.Year
+                select w.Year).First();
+        }
     }
 }
