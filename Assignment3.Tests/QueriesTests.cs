8d15dfa87016c56f271e90538007a4ecf97164af
PhilipFlyvholm <36300927+PhilipFlyvholm@users.noreply.github.com>
2021-09-27 11:52:32 +0200

    Added wizards (#7)
    * Added wizards
    
    * Fix spaces and test
    
    * First two iterations of linq queries
    
    * Return list of harry potter
    
    * Implented last question
    
    Co-authored-by: Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,96 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Assignment3.Tests
 {
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void Extension_WizardsByRowling()
         {
+            var input = "Rowling";
+            var query = new Queries();
+            List<String> expected = new List<string> { "Harry Potter", "Albus Dumbledore", "Lord Voldemort", "Gellert Grindlewald" };
 
+            IEnumerable<String> listOfWizards = query.getWizardsByAuthorExtension(input);
+
+            Assert.Equal(expected, listOfWizards);
+        }
+
+        [Fact]
+        public void Linq_WizardsByRowling()
+        {
+            var input = "Rowling";
+            var query = new Queries();
+            List<String> expected = new List<string> { "Harry Potter", "Albus Dumbledore", "Lord Voldemort", "Gellert Grindlewald" };
+
+            IEnumerable<String> listOfWizards = query.getWizardsByAuthorLinq(input);
+
+            Assert.Equal(expected, listOfWizards);
+        }
+
+        [Fact]
+        public void Extension_FindFirstSithLordYear()
+        {
+            var query = new Queries();
+            var expected = 1977;
+
+            Assert.Equal(expected, query.getFirstSithLordYearExtension());
+        }
+
+        [Fact]
+        public void Linq_FindFirstSithLordYear()
+        {
+            var query = new Queries();
+            var expected = 1977;
+
+            Assert.Equal(expected, query.getFirstSithLordYearLinq());
+        }
+
+        [Fact]
+        public void Extenstion_ReturnsTupleOfHarryPotter()
+        {
+            //Given
+
+            //When
+            var output = new Queries().getHarryPotterWizardsTupleExtension();
+            //Then
+            var expected = new List<(string, int?)> { ("Harry Potter", 1997), ("Albus Dumbledore", 1997), ("Lord Voldemort", 1997) };
+            Assert.Equal(expected, output);
+        }
+        [Fact]
+        public void Linq_ReturnsTupleOfHarryPotter()
+        {
+            //Given
+
+            //When
+            var output = new Queries().getHarryPotterWizardsTupleLinq();
+            //Then
+            var expected = new List<(string, int?)> { ("Harry Potter", 1997), ("Albus Dumbledore", 1997), ("Lord Voldemort", 1997) };
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void Extenstion_returns_wizard_names_grouped_by_creator()
+        {
+            //Given
+            //When
+            var output = new Queries().getListOfWizardsByCreatorExtensions();
+            //Then
+            var expected = new List<string> { "Sauron", "Gandalf", "Lord Voldemort", "Harry Potter", "Gellert Grindlewald", "Albus Dumbledore", "The red woman", "Yoda", "Darth Vader", "Jaina Proudmoore" };
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void Linq_returns_wizard_names_grouped_by_creator()
+        {
+            //Given
+            //When
+            var output = new Queries().getListOfWizardsByCreatorLinq();
+            //Then
+            var expected = new List<string> { "Sauron", "Gandalf", "Lord Voldemort", "Harry Potter", "Gellert Grindlewald", "Albus Dumbledore", "The red woman", "Yoda", "Darth Vader", "Jaina Proudmoore" };
+            Assert.Equal(expected, output);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Assignment3
 {
     public class Queries
     {
+        public IEnumerable<string> getWizardsByAuthorExtension(string author)
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Creator.Contains(author)).Select(w => w.Name);
+        }
+
+        public IEnumerable<string> getWizardsByAuthorLinq(string author)
+        {
+            var wizards = Wizard.Wizards.Value;
+            return from w in wizards
+                   where w.Creator.Contains(author)
+                   select w.Name;
+        }
+
+        public int? getFirstSithLordYearExtension()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Name.Contains("Darth")).OrderBy(w => w.Year).First().Year;
+        }
+
+        public int? getFirstSithLordYearLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return (from w in wizards
+                    where w.Name.Contains("Darth")
+                    orderby w.Year
+                    select w.Year).First();
+        }
+
+        public IEnumerable<(string, int?)> getHarryPotterWizardsTupleExtension()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Medium.Contains("Harry Potter")).Distinct().Select(w => (w.Name, w.Year));
+        }
+        public IEnumerable<(string, int?)> getHarryPotterWizardsTupleLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return (from w in wizards
+                    where w.Medium.Contains("Harry Potter")
+                    select (w.Name, w.Year)).Distinct();
+        }
+
+        public IEnumerable<string> getListOfWizardsByCreatorExtensions()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.OrderByDescending(w => w.Creator).ThenByDescending(w => w.Name).Select(w => w.Name);
+        }
+        public IEnumerable<string> getListOfWizardsByCreatorLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return from w in wizards
+                   orderby w.Creator descending, w.Name descending
+                   select w.Name;
+        }
     }
 }
